5ee21806010335173b241f24fa618218f7be77c4
Prankes <74581704+Prankes@users.noreply.github.com>
2021-06-25 17:30:23 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,5 @@ if(confirm("This website is Under Construction")) {
   
 }
 else {
-  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=85s");
+  window.location.href("https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=85s");
 }
